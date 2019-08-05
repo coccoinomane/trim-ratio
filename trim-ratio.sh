265bad1a2f6bde397491e7a0209d5ad5c6e6b105
Guido W. Pettinari <guido.pettinari@gmail.com>
2019-08-05 20:20:42 +0200

    Now using force-ar also to do horizontal padding

--- trim-ratio.sh
+++ trim-ratio.sh
@@ -53,6 +53,7 @@ zeropad_rectangle=$__output
 echo "  writing to $zeropad_image"
 [[ $zeropad_rectangle = "$bounding_rectangle" ]] && { echo "Nothing to trim, will exit"; exit; }
 magick "$image" -crop "$zeropad_rectangle" +repage "$zeropad_image"
+assertAspectRatioEqual "$image" "$zeropad_image"
 
 # If no padding was requested we are done 
 (( h_pad <= 0 )) && { echo "  No padding requested, will exit"; exit; }
@@ -64,46 +65,12 @@ HT=$(getH "$zeropad_rectangle")
 xT=$(getX "$zeropad_rectangle")
 yT=$(getY "$zeropad_rectangle")
 
-# Space around the AR image
-ST=$yT
-SB=$(echo "$H-$HT-$ST" | bc -l)
-SL=$xT
-SR=$(echo "$W-$WT-$SL" | bc -l)
-echo "    available space: top=${ST}, bottom=${SB}, left=${SL}, right=${SR}"
-
-# Find maximum possibile horizontal padding
-h_pad_max=$h_pad
-if (( $(echo "$SL<$h_pad_max" | bc -l) )); then
-    h_pad_max=$SL
-    echo "    notice: will reduce h_pad from $h_pad to $h_pad_max to stay in image left boundary"
-fi
-if (( $(echo "$SR<$h_pad_max" | bc -l) )); then
-    h_pad_max=$SR
-    echo "    notice: will reduce h_pad from $h_pad to $h_pad_max to stay in image right boundary"
-fi
-if (( $(echo "$ST<$h_pad_max/$AR" | bc -l) )); then
-    h_pad_max=$(echo "$ST*$AR" | bc -l)
-    echo "    notice: will reduce h_pad from $h_pad to $h_pad_max to stay in image top boundary"
-fi
-if (( $(echo "$SB<$h_pad_max/$AR" | bc -l) )); then
-    h_pad_max=$(echo "$SB*$AR" | bc -l)
-    echo "    notice: will reduce h_pad from $h_pad to $h_pad_max to stay in image bottom boundary"
-fi
-
 # Pad the crop rectangle
-v_pad_max=$(echo "$h_pad_max/$AR" | bc -l)
-WT="$(echo "$WT+2*$h_pad_max" | bc -l)"
-xT="$(echo "$xT-$h_pad_max" | bc -l)"
-HT="$(echo "$HT+2*$v_pad_max" | bc -l)"
-yT="$(echo "$yT-$v_pad_max" | bc -l)"
-
-# Output final image
-padded_rectangle=$(getRectangle "$WT" "$HT" "$xT" "$yT")
+WT=$(min "$W" "$(echo "$WT+2*$h_pad" | bc -l)")
+xT=$(max 0 "$(echo "$xT-$h_pad" | bc -l)")
+forceAspectRatio "$W" "$H" "$(getRectangle "$WT" "$HT" "$xT" "$yT")"
+padded_rectangle=$__output
 echo "  rectangle with pad = $padded_rectangle"
 echo "  writing to $padded_image"
 magick "$image" -crop "$padded_rectangle" +repage "$padded_image"
-
-# Check aspect ratio is the same
-ART=$(magick "$padded_image" -format "%[fx:w/h]" info:)
-AR_tolerance=$(echo "1/$(min "$WT" "$HT")" | bc -l)
-assertEqual "$AR" "$ART" "$AR_tolerance" || echo "  WARNING: Found AR discrepance for $name => AR_in=$AR, AR_out=$ART"
+assertAspectRatioEqual "$image" "$padded_image"
